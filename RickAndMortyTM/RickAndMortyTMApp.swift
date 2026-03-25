26fc019db986e671ce3b422e3afe7c742f513860
Tatiana Mccray <ejamison06@gmail.com>
2026-03-24 19:31:17 -0500

    Initial Commit

---
+++ RickAndMortyTM/RickAndMortyTMApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTMApp.swift
+//  RickAndMortyTM
+//
+//  Created by Tatiana6mo on 3/24/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
